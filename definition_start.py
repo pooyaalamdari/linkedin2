de21aebf7c68b393cc0dc0bbe5d9061c5b0cc14f
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-19 13:19:22 +0330

    play with methods

--- definition_start.py
+++ definition_start.py
@@ -10,21 +10,31 @@ class Book:
 
     # create instance methods
     def getprice(self):
-        return self.price
+        if hasattr(self, '_discount'):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
 
+    def setdiscount(self, amount):
+        self._discount = amount
 
 #create a instance of the class
 b1 = Book('Brave new world', 'John Doe', 385, 85.39)
 b2 = Book('War and peace', 'Leo Tolstoy', 1225, 39.99)
 
-#print class
-print(b1)
+# #print class
+# print(b1)
+# #print property
+# print(b1.title)
+# #print method
+# print(b1.getprice())
 
-#print property
-print(b1.title)
-
-#print method
-print(b1.getprice())
-
+#setting discount
+print('before discount')
+print(b2.getprice())
+print('amount of discount = 0.25')
+b2.setdiscount(0.25)
+print('after discount')
+print(b2.getprice())
 
 
