ab0de6f3712233c101fcf23b87e0ad887ce33b72
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-19 13:02:29 +0330

    print method

--- definition_start.py
+++ definition_start.py
@@ -1,14 +1,30 @@
 #create a basic class
+
 class Book:
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
+        #add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
+
+    # create instance methods
+    def getprice(self):
+        return self.price
+
 
 #create a instance of the class
-b1 = Book('Brave new world')
-b2 = Book('War and peace')
+b1 = Book('Brave new world', 'John Doe', 385, 85.39)
+b2 = Book('War and peace', 'Leo Tolstoy', 1225, 39.99)
 
 #print class
 print(b1)
 
 #print property
 print(b1.title)
+
+#print method
+print(b1.getprice())
+
+
+
