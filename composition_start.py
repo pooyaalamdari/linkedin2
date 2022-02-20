f1c80a253475102ed3e54ae018bc381bd031f2f2
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 00:24:30 +0330

    composition2

--- composition_start.py
+++ composition_start.py
@@ -1,21 +1,44 @@
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author = None):
         self.title = title
         self.price = price
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.author = author
 
         self.chapters = []
 
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def addchapter(self, chapter):
+        self.chapters.append(chapter)
 
-b1 = Book('War and peace', 39.0, 'Leo', 'Tolstoy')
+    def getbookpagecount(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.pagecount
+        return result
 
-b1.addchapter('Chapter1', 125)
-b1.addchapter('Chapter2', 97)
-b1.addchapter('Chapter', 143)
+class Author:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+class Chapter:
+    def __init__(self, name, pagecount):
+        self.name = name
+        self.pagecount = pagecount
+
+auth = Author( 'Leo', 'Tolstoy')
+b1 = Book('War and peace', 39.0, auth)
+
+b1.addchapter(Chapter('Chapter1', 125))
+b1.addchapter(Chapter('Chapter2', 97))
+b1.addchapter(Chapter('Chapter', 143))
 
 print(b1.title)
+print(b1.author)
+print(b1.getbookpagecount())
+
+print(auth)
