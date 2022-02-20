f762d1a87e5d7cde7ed52eebe4d63da6de8e3983
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-20 14:39:28 +0330

    multiple3

--- multiple.py
+++ multiple.py
@@ -4,12 +4,14 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = 'foo'
+        self.name = 'Class A'
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = 'bar'
+        self.name = 'Class B'
 
 
 class C(A,B):
@@ -19,6 +21,7 @@ class C(A,B):
     def showprops(self):
         print(self.foo)
         print(self.bar)
+        print(self.name)
 
 
 c = C()
