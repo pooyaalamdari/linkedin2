74a740b5110a8ac66912ef356be2b89960e796c4
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 15:57:28 +0330

    dataclass2

--- dataclass.py
+++ dataclass.py
@@ -1,13 +1,18 @@
+from dataclasses import dataclass
 
-
+@dataclass
 class Book:
-    def __init__(self, title, author, pages, price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
+    title : str
+    author : str
+    pages : int
+    price : float
 
 
 # create some instances
 b1 = Book('War and Peace', 'Leo Tolstoy', 1200, 59.39)
-b2 = Book('Criminal and Punishment', 'Feo Dostoevsky', 800, 40)
+b2 = Book('Criminal and Punishment', 'Feo Dostoevsky', 800, 40.39)
+
+print(b1.title)
+print('------')
+print(b2.author)
+
