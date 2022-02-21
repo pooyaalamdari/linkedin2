2bb054792091917197ffe130bde304301dafd9d0
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 13:15:07 +0330

    magicstr3

--- magicstr.py
+++ magicstr.py
@@ -11,9 +11,16 @@ class Book:
     def __str__(self):
         return f'{self.title} by {self.author}, costs {self.price}'
 
+    # use the __repl__ method
+    def __repr__(self):
+        return f'title={self.title}, author={self.author}, price={self.price}'
+
+
 
 b1 = Book('War and Peace', 'Leo Tolstoy', 99.39)
 b2 = Book('book example 2', 'John Doe', 9)
 
 print(b1)
 print(b2)
+print(str(b1))
+print(repr(b2))
