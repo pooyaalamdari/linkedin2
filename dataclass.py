7cf5e35afb6557bd0414048d072089a4a4f37dfc
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 15:59:35 +0330

    dataclass3

--- dataclass.py
+++ dataclass.py
@@ -11,8 +11,11 @@ class Book:
 # create some instances
 b1 = Book('War and Peace', 'Leo Tolstoy', 1200, 59.39)
 b2 = Book('Criminal and Punishment', 'Feo Dostoevsky', 800, 40.39)
+b3 = Book('Go fuck yourself', 'write by me', 666, 85)
 
 print(b1.title)
 print('------')
 print(b2.author)
+print('------')
+print(b3.price)
 
