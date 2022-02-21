b7d6b41d1535084ca6512abf5efe948e90c4bbd0
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 13:28:41 +0330

    magiceq

---
+++ magiceq.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author, price):
+        super().__init__()
+        self.title = title
+        self.author = author
+        self.price = price
+
+
+b1 = Book('war and zone', 'alex', 39)
+b2 = Book('book2', 'person2', 49.99)
+b3 = Book('war and zone', 'alex', 39)
+b4 = Book('book4', 'person4', 50)
+
+
