9039c7e5bf12aa8d29c97dee71f1b95fb9e0e221
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 14:45:54 +0330

    setattr

--- magicattr.py
+++ magicattr.py
@@ -17,12 +17,23 @@ class Book:
             return p - (p * d)
         return super().__getattribute__(name)
 
+    # __setattr
+    def __setattr__(self, key, value):
+        if key == 'price':
+            if type(value) is not float:
+                raise ValueError('The price attr must be a float')
+        return super().__setattr__(key, value)
+
 
 b1 = Book('War and Peace', 'Leo Tolstoy', 39.95)
 b2 = Book('criminal and punishment', 'feodor dostayevsky', 40)
 
-# 38.95 - (38.95 * 0.1)
-b1.price = 38.95
-print(b1)
-print('------')
-print(b1.price)
+# # 38.95 - (38.95 * 0.1)
+# b1.price = 38.95
+# print(b1)
+# print('------')
+# print(b1.price)
+
+b2.price = 40
+print(b2)
+
