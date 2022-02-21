097f941585e542828322826d113f63c61f5191a2
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 16:04:41 +0330

    dataclass 4

--- dataclass.py
+++ dataclass.py
@@ -12,10 +12,18 @@ class Book:
 b1 = Book('War and Peace', 'Leo Tolstoy', 1200, 59.39)
 b2 = Book('Criminal and Punishment', 'Feo Dostoevsky', 800, 40.39)
 b3 = Book('Go fuck yourself', 'write by me', 666, 85)
+b4 = Book('Go fuck yourself', 'write by me', 666, 85)
 
-print(b1.title)
-print('------')
-print(b2.author)
-print('------')
-print(b3.price)
+# with dataclass we can print book itself!
+print(b1)
+
+# with dataclass we can compare objects together
+print(b3 == b4)
+
+
+# print(b1.title)
+# print('------')
+# print(b2.author)
+# print('------')
+# print(b3.price)
 
