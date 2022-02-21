e64e972365be8b84501c58d5ced6b36daf0477ed
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 19:49:05 +0330

    default value

--- datadefault.py
+++ datadefault.py
@@ -7,9 +7,9 @@ class Book:
     title: str = 'No Title'
     author: str = 'No Author'
     pages: int = 0
-    price: float = 0.0
+    price: float = 10.0 # default
 
 
 # define obj without argument
-b1 = Book()
+b1 = Book('Criminal and Punishment', 'Feo Dostoevsky', 600)
 print(b1)
