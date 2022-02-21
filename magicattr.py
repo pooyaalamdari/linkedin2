2f3286c75be6a0bc6c01a4c41a7bccf07916e871
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-21 14:38:35 +0330

    get attribute

--- magicattr.py
+++ magicattr.py
@@ -9,6 +9,20 @@ class Book:
     def __str__(self):
         return f'{self.title} by {self.author} costs {self.price}'
 
+    # __getattribute__ for retrieved attribute
+    def __getattribute__(self, name):
+        if name == 'price':
+            p = super().__getattribute__('price')
+            d = super().__getattribute__('_discount')
+            return p - (p * d)
+        return super().__getattribute__(name)
 
-b1 = Book('War and Peace', 'Leo Tolstoy', 39.29)
+
+b1 = Book('War and Peace', 'Leo Tolstoy', 39.95)
 b2 = Book('criminal and punishment', 'feodor dostayevsky', 40)
+
+# 38.95 - (38.95 * 0.1)
+b1.price = 38.95
+print(b1)
+print('------')
+print(b1.price)
