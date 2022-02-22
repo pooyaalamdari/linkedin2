d9895b27f64818e4e26c17a9912ef6412d3fd4c8
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-22 12:28:40 +0330

    immutable2

--- immutable.py
+++ immutable.py
@@ -6,10 +6,16 @@ class ImmutableClass:
     value1: str = 'Value1'
     value2: int = 0
 
+    def somefunc(self, newval):
+        self.value2 = newval
 
 obj = ImmutableClass()
 print(obj.value1)
 
-# if we run we got an error 
+# if we run we got an error
 obj.value1 = 'Another value'
 print(obj.value1)
+
+# if run we got an Error again 
+obj.somefunc(123)
+
